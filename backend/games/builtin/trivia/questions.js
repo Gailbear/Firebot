65f95c5c6eb915c96b620bdb06034ae64db0149f
Gail Terman <Gailbear@users.noreply.github.com>
2021-09-10 07:30:49 -0400

    Create questions.js
    add internal questions file

---
+++ backend/games/builtin/trivia/questions.js
@@ -0,0 +1,10 @@
+module.exports = {
+  sociology: [
+    // question: the text of the question
+    // difficulty: one of "easy", "medium", or "hard". Capitalization matters.
+    // answers: an array of wrong and right answers. Capitalization should be as you want it displayed.
+    // correctIndex: the 1-based index (position) of your correct answer. The first item is 1, the second is 2, etc.
+    { question: "Sample Question 1", difficulty: "easy", answers: [ "Wrong answer 1", "Correct answer", "Wrong answer 2"], correctIndex: 2 },
+    { question: "Sample Question 2", difficulty: "hard", answers: [ "True", "False" ], correctIndex: 1 },
+  ]
+}
